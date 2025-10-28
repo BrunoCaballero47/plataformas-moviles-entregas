aa27aed5713818b5548d5caf91374536c9f67e9d
Bruno <135883982+BrunoCaballero47@users.noreply.github.com>
2025-10-28 15:39:56 -0300

    Delete tp-16/script.js

--- tp-16/script.js
+++
@@ -1,7 +0,0 @@
-function obtenerPokemones(){
-    fetch("https://pokeapi.co/api/v2/pokemon")
-.then((response) => response.json())  // Convierte los datos a json
-.then(data => {  // Crea una nueva funcion, pasando los datos obtenidos como parámetro
-    console.log(data)  // (O cualquier código js)
-})
-}
